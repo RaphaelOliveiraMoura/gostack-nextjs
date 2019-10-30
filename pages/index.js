70d02448867528c4fcbde7c20ad2aaa8bec961e2
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-10-30 15:51:20 -0300

    feat: Add head with next

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
+
 import Link from 'next/link';
+import Head from 'next/head';
 
 const Home = () => (
   <>
+    <Head>
+      <title>Home</title>
+    </Head>
     <div>Hello World</div>
     <Link href="/users">
       <a>Usuários</a>
