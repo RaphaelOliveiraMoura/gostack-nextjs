de8b770bbfe16530189dc17eafc17f1b62e69f2c
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-10-31 13:34:44 -0300

    feat: Setting root dir with ~

--- pages/home.js
+++ pages/home.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import Link from 'next/link';
 import Head from 'next/head';
 
-import withAnalytics from '../src/hocs/withAnalytics';
+import withAnalytics from '~/hocs/withAnalytics';
 
 const Title = styled.h1`
   color: #7159c1;
@@ -16,6 +16,7 @@ const Home = () => (
     <Head>
       <title>Home</title>
     </Head>
+    <img src="static/profile.jpeg" alt="Profile" width="200" />
     <Title>Hello World</Title>
     <Link href="/users">
       <a>Usuários</a>
