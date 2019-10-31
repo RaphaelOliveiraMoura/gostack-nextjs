07c9d540cc9bc138912cf5599f81cfebbf0cb871
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-10-31 13:12:22 -0300

    feat: Add styles

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,23 @@
 import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    );
+
+    const styleTags = sheet.getStyleElement();
+
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <html>
         <Head>
-          <style>{`body { background: #7159c1 }`}</style>
+          <style>{this.props.styleTags}</style>
         </Head>
         <body>
           <Main />
--- pages/home.js
+++ pages/home.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Link from 'next/link';
 import Head from 'next/head';
 
 import withAnalytics from '../src/hocs/withAnalytics';
 
+const Title = styled.h1`
+  color: #7159c1;
+  font-size: 40px;
+`;
+
 const Home = () => (
   <>
     <Head>
       <title>Home</title>
     </Head>
-    <div>Hello World</div>
+    <Title>Hello World</Title>
     <Link href="/users">
       <a>Usuários</a>
     </Link>
